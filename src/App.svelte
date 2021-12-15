0a056417e2a1e4f7dc769a2b284abd0ca8f02e8d
Daniel G <daniel@grundel.me>
2021-12-15 11:36:38 -0800

    it builds and apk works! (+ fonts)

--- src/App.svelte
+++ src/App.svelte
@@ -8,26 +8,8 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<p>Clicks: {count}</p>
 	<button on:click={() => { count += 1; }}>Click this</button>
+
+	<pre>
+		Here's a code block (sort of!)
+	</pre>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
