196492cd26ed37891859c285abd68a4b567fd945
Daniel G <daniel@grundel.me>
2021-12-15 11:59:32 -0800

    toy editor

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
-	export let name: string;
-	let count = 0;
+    import Editor from "./Editor.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p>Clicks: {count}</p>
-	<button on:click={() => { count += 1; }}>Click this</button>
-
-	<pre>
-		Here's a code block (sort of!)
-	</pre>
+    <Editor/>
 </main>
---
+++ src/Editor.svelte
@@ -0,0 +1,32 @@
+<script>
+    let innerHTML;
+</script>
+
+<div class="frame">
+    <div class="editable" contenteditable bind:innerHTML>
+        Hello, world!
+    </div>
+    
+    <pre>{innerHTML}</pre>    
+</div>
+
+
+<style>
+    .frame {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 2fr 1fr;
+    }
+
+    .editable {
+        outline: none;
+        min-height: 1em;
+        padding: .75em;
+    }
+</style>
