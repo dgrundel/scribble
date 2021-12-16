de450c438a2b6d65d9ef97f13da749dd7090c93d
Daniel G <daniel@grundel.me>
2021-12-16 10:59:57 -0800

    better transitions for drawer and overlay

--- src/Drawer.svelte
+++ src/Drawer.svelte
@@ -19,27 +19,59 @@
 
 <style>
     .overlay {
+        --transition-duration: 175ms;
+
         position: fixed;
         top: 0;
         bottom: 0;
         left: -100vw;
         width: 100vw;
         z-index: 9999;
-        transition: left 200ms ease-in-out;
+        transition-delay: var(--transition-duration);
+        transition-duration: 0;
+        transition-property: left;
     }
 
     .overlay.open {
         left: 0;
+        transition-delay: unset;
+    }
+
+    .overlay::before {
+        content: ' ';
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background-color: #000;
+        opacity: 0;
+        transition-duration: var(--transition-duration);
+        transition-property: opacity;
+    }
+
+    .overlay.open::before {
+        opacity: 0.6;
     }
 
     .drawer {
         position: absolute;
         top: 0;
         bottom: 0;
-        left: 0;
+        left: -85vw;
         width: 85vw;
         background-color: #282828;
         box-shadow: 0 0 2rem rgba(0,0,0,0.4);
+        z-index: 1;
+        transition-delay: 0;
+        transition-duration: var(--transition-duration);
+        transition-property: left;
+        transition-timing-function: ease-in-out;
+    }
+
+    .open .drawer {
+        left: 0;
     }
 
     .close-button {
