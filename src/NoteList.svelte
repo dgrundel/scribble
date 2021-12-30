7d00b8cf55189caa8f45cf18ff2143894ca07878
Daniel Grundel <grundeld@amazon.com>
2021-12-29 20:59:28 -0800

    removable tags in list view, no result message

--- src/NoteList.svelte
+++ src/NoteList.svelte
@@ -5,7 +5,9 @@
     import Signature from "tabler-icons-svelte/icons/Signature.svelte";
     import SortAscending from "tabler-icons-svelte/icons/SortAscending.svelte";
     import SortDescending from "tabler-icons-svelte/icons/SortDescending.svelte";
+    import Tag from "tabler-icons-svelte/icons/Tag.svelte";
     import Star from "tabler-icons-svelte/icons/Star.svelte";
+    import X from "tabler-icons-svelte/icons/X.svelte";
     import NoteListItem from "./NoteListItem.svelte";
     import { goToPage } from "./router";
     import { getNoteStore, Note, NoteFilters } from "./Note";
@@ -37,13 +39,28 @@ import Icon from "./Icon.svelte";
 <Layout {openMenu}>
     <svelte:fragment slot="toolbar">
         <span class="divider"></span>
-        
+
         <button aria-label="Starred" class={filters.starred === true ? 'active' : ''} 
             on:click={() => filters.starred = filters.starred === true ? undefined : true }>
             <Icon icon={Star}/>
         </button>
 
-        <span class="toolbar-text">Sort by:</span>
+        <span class="divider"></span>
+
+        {#if filters.tags && filters.tags.length}
+            <span class="toolbar-text">
+                Tags
+            </span>
+
+            <button aria-label="Remove tag filter" class="active" on:click={() => filters.tags = undefined } data-prefix={filters.tags.join(', ')}>
+                <Icon icon={X}/>
+            </button>
+
+            <span class="divider"></span>
+        {/if}
+
+        <span class="toolbar-text">Sort</span>
+
         <button aria-label="Title" class={filters.sort.col === 'title' ? 'active' : ''} on:click={() => filters.sort.col = 'title'}>
             <Icon icon={Signature}/>
         </button>
@@ -68,8 +85,19 @@ import Icon from "./Icon.svelte";
                     {note.title}
                 </NoteListItem>
             {/each}
+            {#if notes.length === 0}
+                <p class="no-results">No items.</p>
+            {/if}
         {:else}
             <Spinner/>
         {/if}
     </svelte:fragment>
 </Layout>
+
+<style>
+    .no-results {
+        text-align: center;
+        padding: 0 var(--padding);
+        opacity: 0.8;
+    }
+</style>
