154281ce871260b1b8658a72f6805da0a9d34403
Daniel Grundel <grundeld@amazon.com>
2021-12-29 20:26:04 -0800

    markdown config

--- src/markdown.ts
+++ src/markdown.ts
@@ -2,7 +2,9 @@ import markdownIt from 'markdown-it';
 import turndown from 'turndown';
 
 const mdParser = markdownIt({ 
-    html: true,
+    html: true, // Enable HTML tags in source
+    // breaks: true, // Convert '\n' in paragraphs into <br>
+    // typographer: true, // https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
 });
 const mdRenderer = new turndown({
     headingStyle: 'atx',
